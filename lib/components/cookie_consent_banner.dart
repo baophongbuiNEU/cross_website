24f3254a1fbdd3108c3f214eec5b64245deb1a11
baophongbui <phongtin765@gmail.com>
2025-05-23 13:52:56 +0700

    fix_cookies

--- lib/components/cookie_consent_banner.dart
+++ lib/components/cookie_consent_banner.dart
@@ -1,3 +1,5 @@
+import 'package:cross_website/components/common/size_box_component.dart';
+import 'package:cross_website/constants/app_colors.dart';
 import 'package:cross_website/language/language_manager.dart';
 import 'package:jaspr/jaspr.dart';
 import 'package:jaspr_riverpod/jaspr_riverpod.dart';
@@ -29,19 +31,22 @@ class CookieConsentBanner extends StatelessComponent {
 
     yield div(
       styles: Styles(
+        shadow: BoxShadow(
+            color: Color.hex(shadowColor),
+            blur: 12.px,
+            offsetX: 0.px,
+            offsetY: 4.px),
+        backgroundColor: AppColors.textBlack,
         raw: {
           'position': 'fixed',
           'bottom': '30px',
           'left': '50%',
           'transform': 'translateX(-50%)',
-          'background-color': backgroundColor.value,
-          'color': textColor.value,
           'padding': '15px 15px',
           'border-radius': '16px',
           'display': 'flex',
           'align-items': 'center',
           'justify-content': 'space-between',
-          'box-shadow': '0 4px 12px $shadowColor',
           'font-family': 'Arial, sans-serif',
           'z-index': '1000',
           'max-width': '800px',
@@ -60,21 +65,44 @@ class CookieConsentBanner extends StatelessComponent {
             },
           ),
           [
-            text('We use cookies to improve your experience.'),
-            span([
-              text('Learn more in our '),
-              a(
-                href: '/privacy-policy',
+            p(
                 styles: Styles(
-                  raw: {
-                    'color': accentColor.value,
-                    'text-decoration': 'none',
-                  },
+                    display: Display.flex,
+                    padding: Spacing.all(0.px),
+                    margin: Spacing.all(0.px),
+                    flexWrap: FlexWrap.wrap,
+                    color: AppColors.backgroundTheme),
+                [text('We use cookies to improve your experience.')]),
+            div(
+                styles: Styles(
+                  display: Display.flex,
+                  flexDirection: FlexDirection.row,
+                  flexWrap: FlexWrap.wrap,
                 ),
-                [text('Privacy Policy')],
-              ),
-              text('.'),
-            ]),
+                [
+                  p(
+                      styles: Styles(
+                          display: Display.flex,
+                          padding: Spacing.all(0.px),
+                          margin: Spacing.all(0.px),
+                          flexWrap: FlexWrap.wrap,
+                          color: AppColors.backgroundTheme),
+                      [
+                        text('Learn more in our'),
+                      ]),
+                  SizeBoxComponent(width: 5),
+                  a(
+                    href: '/privacy-policy',
+                    styles: Styles(
+                      raw: {
+                        'color': accentColor.value,
+                        'text-decoration': 'none',
+                      },
+                    ),
+                    [text('Privacy Policy')],
+                  ),
+                  text('.'),
+                ]),
           ],
         ),
         div(
@@ -87,14 +115,14 @@ class CookieConsentBanner extends StatelessComponent {
           [
             button(
               styles: Styles(
+                border: Border(color: AppColors.backgroundTheme, width: 1.px),
+                cursor: Cursor.pointer,
+                color: AppColors.backgroundTheme,
+                backgroundColor: Colors.transparent,
                 raw: {
-                  'background-color': 'transparent',
-                  'color': textColor.value,
-                  'border': '1px solid ${textColor.value}',
                   'padding': '8px 20px',
                   'border-radius': '24px',
-                  'cursor': 'pointer',
-                  'font-weight': 'bold',
+                  'font-weight': 'bold'
                 },
               ),
               events: {
@@ -107,14 +135,14 @@ class CookieConsentBanner extends StatelessComponent {
             ),
             button(
               styles: Styles(
+                color: AppColors.textBlack,
+                backgroundColor: AppColors.backgroundTheme,
                 raw: {
-                  'background-color': textColor.value,
-                  'color': backgroundColor.value,
                   'border': 'none',
                   'padding': '8px 20px',
                   'border-radius': '24px',
                   'cursor': 'pointer',
-                  'font-weight': 'bold',
+                  'font-weight': 'bold'
                 },
               ),
               events: {
